d4181c117c4cbd2ea4c61af17d297c5887e344db
mizan <mizanurrahmanmizan934@gmail.com>
2024-10-21 12:46:14 +0600

    router setup

--- travel_journal/src/App.jsx
+++ travel_journal/src/App.jsx
@@ -1,16 +1,26 @@
+//import { Root } from 'postcss';
 import React from 'react';
 //import './App.css';
-import Navbar from './components/header/navbar';
+//import Navbar from './components/header/navbar';
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+
+import Layout from './layout/Layout';
+
 function App() {
   
-  return (
-    <>
-      <div className=''>
-        <Navbar/>
-      </div>
-      
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        
+    </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
